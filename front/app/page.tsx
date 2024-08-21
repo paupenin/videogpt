b22b312b4a26bdb847ff0c8c5246e7fd5b365eec
Pau Penin <paupenin@gmail.com>
2024-08-21 18:21:36 +0200

    [FE] Video upload and search placeholder

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,112 +1,116 @@
-import Image from "next/image";
+"use client";
+
+import React from "react";
+
+const VIDEO_API = "http://127.0.0.1:8000";
 
 export default function Home() {
+  const [videoURL, setVideoURL] = React.useState<string | null>(null);
+  const [searchText, setSearchText] = React.useState<string>("");
+
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const videoRef = React.useRef<HTMLVideoElement>(null);
+
+  const handleUpload = async (videoUpload: File | undefined) => {
+    console.log(videoUpload);
+    if (!videoUpload) return;
+
+    const formData = new FormData();
+    formData.append("file", videoUpload);
+
+    const response = await fetch(`${VIDEO_API}/video/`, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (response.ok) {
+      const { url } = await response.json();
+      setVideoURL(url);
+    } else {
+      // TODO: Handle error and show message
+      alert("Error uploading video");
+    }
+  }
+
+  const handleSearch = async () => {
+    if (searchText === "") return;
+
+    const url = `${VIDEO_API}/video/${videoURL}/search?text=${encodeURIComponent(searchText)}`;
+
+    const response = await fetch(url, {
+      method: "POST",
+    });
+
+    if (response.ok) {
+      const { time } = await response.json();
+      console.log("Time found:", time);
+
+      if (time) {
+        console.log(videoRef.current!.currentTime)
+        videoRef.current!.currentTime = time;
+      } else {
+        alert("Text not found in video");
+      }
+    } else {
+      console.error('Search failed:', response.statusText);
+      return;
+    }
+  };
+
+  console.log("videoURL", videoURL);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <main className="flex h-screen flex-col items-center justify-between">
+      <div className="z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex">
+        <p className="fixed text-2xl font-bold left-0 top-0 flex w-full justify-center pb-6 pt-8 lg:static lg:w-auto">
+          VideoGPT
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center lg:static lg:size-auto">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://www.linkedin.com/in/paupenin/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            By Pau Penin
           </a>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="flex flex-col items-center justify-center w-full h-full gap-4 grow max-h-screen">
+        <button className="text-black bg-white rounded px-4 py-2" onClick={() => inputRef.current!.click()}>
+          Upload video
+        </button>
+        <input
+          ref={inputRef}
+          type="file"
+          accept="video/*"
+          className="hidden"
+          onChange={(e) => handleUpload(e.target.files?.[0])}
         />
-      </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {videoURL && (
+          <>
+            <div className="flex max-w-xl">
+              <input
+                type="text"
+                placeholder="Use this text to search in the video"
+                className="w-full p-2 rounded-l"
+                value={searchText}
+                onChange={(e) => {console.log(e.target.value); setSearchText(e.target.value)}}
+              />
+              <button className="text-black bg-slate-100 rounded-r px-4 py-2" onClick={handleSearch}>
+                Search
+              </button>
+            </div>
+            <video
+              style={{ maxHeight: "calc(100vh - 250px)" }}
+              controls
+              src={`${VIDEO_API}/media/${videoURL}`}
+              ref={videoRef}
+            />
+          </>
+        )}
       </div>
     </main>
   );
